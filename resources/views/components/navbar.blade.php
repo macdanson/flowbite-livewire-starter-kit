b546adcd37e10ea3fbb688f65c849b0b301381c2
macdanson <macdanson2@gmail.com>
2025-05-23 16:06:48 +0300

    Cleaning dead links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,32 +10,28 @@
                 </svg>
                 <span class="sr-only">Toggle sidebar</span>
             </button>
-            <a href="" class="flex items-center justify-between mr-4">
+            <a href="{{ route('dashboard') }}" class="flex items-center justify-between mr-4">
                 <img src="https://flowbite.s3.amazonaws.com/logo.svg" class="mr-3 h-8" alt="Flowbite Logo"/>
                 <span class="self-center text-2xl font-bold whitespace-nowrap text-white">My App</span>
             </a>
         </div>
         <div class="flex items-center lg:order-2">
             <!-- Notifications -->
-            <button type="button" data-dropdown-toggle="notification-dropdown" class="p-2 mr-1 text-white rounded-lg hover:text-[#009549] hover:bg-gray-100">
+            <button type="button" data-dropdown-toggle="notification-dropdown" class="relative p-2 mr-1 text-white rounded-lg hover:text-[#009549] hover:bg-gray-100">
                 <span class="sr-only">View notifications</span>
                 <!-- Bell icon -->
-                <svg
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"
-                    ></path>
+                <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
                 </svg>
+                <!-- Badge -->
+                <span class="absolute top-0 right-0 flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
+                    1 <!-- Replace with dynamic value -->
+                </span>
             </button>
 
             <!-- Dropdown menu -->
             <div class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg" id="notification-dropdown">
-                <div class="block py-2 px-4 text-base font-medium text-gray-700 bg-gray-50">
+                <div class="block py-2 px-4 text-base font-medium text-gray-700 bg-gray-200">
                     Notifications
                 </div>
                 <div>
@@ -79,17 +75,12 @@
                 </svg>
             </button>
             <!-- Dropdown menu -->
-            <div class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg" id="apps-dropdown">
-                <div
-                    class="block py-2 px-4 text-base font-medium text-gray-700 bg-gray-50"
-                >
-                    Other Apps Shortcut
+            <div class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded-lg divide-y divide-green-200 shadow-sm" id="apps-dropdown">
+                <div class="block py-2 px-4 text-base font-medium text-gray-700 bg-gray-200">
+                    Application Shortcut
                 </div>
                 <div class="grid grid-cols-3 gap-4 p-4">
-                    <a
-                        href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
-                    >
+                    <a href="#" class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group">
                         <svg
                             aria-hidden="true"
                             class="mx-auto mb-1 w-7 h-7 text-gray-400 group-hover:text-gray-500"
@@ -107,7 +98,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -124,7 +115,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -143,7 +134,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -164,7 +155,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -185,7 +176,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -207,7 +198,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -231,7 +222,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -252,7 +243,7 @@
                     </a>
                     <a
                         href="#"
-                        class="block p-4 text-center rounded-lg hover:bg-gray-100 group"
+                        class="block p-4 text-center rounded-lg bg-gray-100 hover:bg-gray-200 group"
                     >
                         <svg
                             aria-hidden="true"
@@ -290,38 +281,29 @@
                 />
             </button>
             <!-- Dropdown menu -->
-            <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow" id="dropdown">
+            <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-300 shadow-sm" id="dropdown">
                 <div class="py-3 px-4">
-              <span class="block text-sm font-semibold text-gray-900">{{ auth()->user()->name }}</span>
+                    <span class="block font-semibold text-gray-900">{{ auth()->user()->name }}</span>
                     <span class="block text-sm text-gray-900 truncate">{{ auth()->user()->email }}</span>
                 </div>
+
                 <ul class="py-1 text-gray-700" aria-labelledby="dropdown">
                     <li>
-                        <a href="{{ route('settings.profile') }}" class="block py-2 px-4 text-sm hover:bg-gray-100">My profile</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100">Account settings</a>
-                    </li>
-                </ul>
-                <ul class="py-1 text-gray-700" aria-labelledby="dropdown">
-                    <li>
-                        <a href="#" class="flex items-center py-2 px-4 text-sm hover:bg-gray-100">
-                            <svg class="mr-2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path>
+                        <a href="{{ route('settings.profile') }}" class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100 hover:font-medium">
+                            <span class="flex items-center">
+                                <svg class="mr-2 w-5 h-5 text-primary-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                             </svg>
-                            My likes
+                                Account Settings
+                            </span>
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                            </svg>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="flex items-center py-2 px-4 text-sm hover:bg-gray-100">
-                            <svg class="mr-2 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
-                            </svg>
-                            Collections
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100">
+                        <a href="#" class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100 hover:font-medium">
                             <span class="flex items-center">
                                 <svg aria-hidden="true" class="mr-2 w-5 h-5 text-primary-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd"></path>
@@ -334,10 +316,10 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('logout') }}" class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100">
+                        <a href="{{ route('logout') }}" class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100 hover:text-red-600 hover:font-medium">
                             <span class="flex items-center">
-                                <svg aria-hidden="true" class="mr-2 w-5 h-5 text-primary-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd"></path>
+                                <svg class="mr-2 w-5 h-5 text-primary-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/>
                                 </svg>
                                 Sign Out
                             </span>
